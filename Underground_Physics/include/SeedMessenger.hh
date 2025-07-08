fa8a3562aeda80ec810a2c8fddc77fa81a10310a
Kaitlyn Konopka <kpk5732@psu.edu>
2025-07-08 16:06:39 -0400

    Update SeedMessenger.hh
    "isNull" is no longer valid for this version of Geant4, "empty" is now the updated termage

--- Underground_Physics/include/SeedMessenger.hh
+++ Underground_Physics/include/SeedMessenger.hh
@@ -34,7 +34,7 @@ class SeedMessenger: public G4UImessenger {
       G4int idx=0;
       G4long seeds[100];
       G4String vl;
-      while(!(vl=next()).isNull()) {
+      while(!(vl=next()).empty()) {
         seeds[idx] = StoL(vl);
         idx++;
       }
